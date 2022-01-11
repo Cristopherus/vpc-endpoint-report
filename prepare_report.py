67d1b112690c8af327898d053f409a5a6d3462f9
Krzysztof Dziuban <Krzysztof.Dziuban@abacai.com>
2022-01-11 21:52:07 +0100

    spellcheck for python

--- prepare_report.py
+++ prepare_report.py
@@ -1,16 +1,23 @@
+'''
+Creates tex file from templates with data from csv files
+'''
 import logging
 import glob
 from jinja2 import Template
 
 def main():
-	envs = []
-	for file in glob.glob("*.csv"):
-		envs.append(file.removesuffix('.csv'))
-	with open('endpoints.tex.j2') as f:
-		template = Template(f.read())
-		template.stream(envs=envs).dump('endpoints.tex')
+    '''
+    Read names of csv files and genereate tex file from template with using those csv-file names
+    '''
+    envs = []
+    logging.info("Checking envs number and names")
+    for file in glob.glob("*.csv"):
+        envs.append(file.removesuffix('.csv'))
+    logging.info("Creating tex file from template")
+    with open('endpoints.tex.j2',encoding='utf-8') as file:
+        template = Template(file.read())
+        template.stream(envs=envs).dump('endpoints.tex')
 
 if __name__ == '__main__':
-	logging.basicConfig(level=logging.INFO)
-	
-	main()
+    logging.basicConfig(level=logging.INFO)
+    main()
